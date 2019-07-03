4c0021d74641d1316e912194446c6b1fa76a2463
caiyuli <caiyuli@songguo7.com>
2019-07-03 13:47:44 +0800

    练习git

---
+++ src/main/java/com/cai/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.cai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cai/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.cai.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ValueConstants;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = {"/index"})
+    public String index(){
+
+        return "index";
+    }
+
+}
